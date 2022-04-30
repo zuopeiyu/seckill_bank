ffeb2eb1ffcf364cb48e240e77be4f88e8902dcf
threemeet <15934178106@163.com>
2022-04-30 22:16:22 +0800

    添加商品测试

--- src/main/java/com/csse/controller/GoodsController.java
+++ src/main/java/com/csse/controller/GoodsController.java
@@ -1,5 +1,11 @@
 package com.csse.controller;
 
+import com.csse.domain.GoodsEntity;
+import com.csse.result.RespBean;
+import com.csse.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/goods")
 public class GoodsController {
 
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @PostMapping("/saveGoods")
+    public RespBean saveGoods(@RequestBody GoodsEntity goodsEntity){
+        goodsService.save(goodsEntity);
+        return RespBean.success();
+    }
 }
